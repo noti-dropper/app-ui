1772d0698eca7db33edc631755f3c3b095ac696d
HWRdeveloper <neodeveloper12@gmail.com>
2019-10-24 11:26:14 +0900

    ui 변경 및 스플래시 추가

---
+++ app/src/main/java/com/example/mainappdialogversion/FirstOpen.java
@@ -0,0 +1,26 @@
+package com.example.mainappdialogversion;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FirstOpen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_open);
+
+    }
+
+
+}
